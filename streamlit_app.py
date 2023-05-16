f43edaba5b3fcdf72f391678782ba91693bb7671
Omar S. Gómez <omargomez2@gmail.com>
2023-05-16 18:12:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,20 +11,20 @@ import streamlit as st
 st.title('Enfoque de cifrado de objetos JSON utilizando sincronización caótica a partir del análisis' +
                 ' de un conjunto de atractores IDIPI-28')
 
-txt = """Resumen. En la actualidad en mayor o menor medida los productos software desarrollados + 
-         se encuentran expuestos al Internet.  Internamente estos productos están conformados + 
-         por componentes software los cuales, muchos de ellos, se intercomunican a través del +
-         Internet, enviando y recibiendo información. Un formato común para el intercambio de + 
-         información entre estos componentes es el formato JSON, Sin bien existen algunos enfoque +
-         para asegurar la información en este formato, se ha observado algunas amenazas que implican + 
-         el acceso no autorizado a la información contenida en los objetos JSON. Con el fin de ofrecer + 
-         una alternativa al actual enfoque de cifrado de objetos JSON en la presente investigación se + 
-         propone el desarrollo de un enfoque de cifrado de objetos JSON utilizando sincronización caótica +
-         a partir de la selección de un conjunto de atractores. El enfoque a desarrollar se pretende pueda +
-         ser utilizado en el ámbito académico por docentes e investigadores, así como por profesionales en la + 
-         industria de software. La presente investigación se llevará a cabo en la ESPOCH así como se tiene previsto + 
-         la realización de una estancia de investigación. Los recursos requeridos serán financiados por ESPOCH, y serán +
-         ejecutados por los miembros participantes en este proyecto (dos recursos coordinador y miembro de proyecto). El + 
+txt = """Resumen. En la actualidad en mayor o menor medida los productos software desarrollados
+         se encuentran expuestos al Internet.  Internamente estos productos están conformados 
+         por componentes software los cuales, muchos de ellos, se intercomunican a través del
+         Internet, enviando y recibiendo información. Un formato común para el intercambio de
+         información entre estos componentes es el formato JSON, Sin bien existen algunos enfoque
+         para asegurar la información en este formato, se ha observado algunas amenazas que implican 
+         el acceso no autorizado a la información contenida en los objetos JSON. Con el fin de ofrecer
+         una alternativa al actual enfoque de cifrado de objetos JSON en la presente investigación se 
+         propone el desarrollo de un enfoque de cifrado de objetos JSON utilizando sincronización caótica
+         a partir de la selección de un conjunto de atractores. El enfoque a desarrollar se pretende pueda
+         ser utilizado en el ámbito académico por docentes e investigadores, así como por profesionales en la 
+         industria de software. La presente investigación se llevará a cabo en la ESPOCH así como se tiene previsto
+         la realización de una estancia de investigación. Los recursos requeridos serán financiados por ESPOCH, y serán
+         ejecutados por los miembros participantes en este proyecto (dos recursos coordinador y miembro de proyecto). El 
          plazo previsto de ejecución es de veinticuatro meses."""
 
 
