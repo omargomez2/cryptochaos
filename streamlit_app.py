a8dd17993b31e21b4c67a002a04d498d76a15269
Omar S. Gómez <omargomez2@gmail.com>
2023-05-16 17:44:58 -0500

    v0

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+#--------------------
+# 
+# Authors: OG, RR, DP, MR
+# Description: 
+# url: 
+#-----------
+
+import streamlit
+
+streamlit.title("Enfoque de cifrado de objetos JSON utilizando sincronización caótica a partir del análisis de un conjunto de atractores")
