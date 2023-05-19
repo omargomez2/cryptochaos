50a12031093b755c18db3491c443a48c787e8752
Omar S. Gómez <omargomez2@gmail.com>
2023-05-19 08:36:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,24 @@ components.html(
     height=600,
 )
 
+st.subheader('Atractor de Rossler')
+components.html(
+    """
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <div style="text-align: justify">
+      El atractor de Rössler describe el comportamiento de un sistema dinámico de tres ecuaciones 
+      diferenciales ordinarias no lineales. Fue estudiado originalmente por Otto Rössler en la década de 1970 y 
+      exhibe dinámicas caóticas y propiedades fractales. El atractor de Rössler se puede utilizar para 
+      modelar el equilibrio en reacciones químicas y otros fenómenos. Las ecuaciones que definen el atractor de Rössler son:
+      donde A, B y C son los parametros de equilibrio. El atractor de Rössler tiene forma de espiral que se tuerce y gira 
+      en tres dimensiones. Tiene algunas similitudes con el atractor de Lorenz, pero es más simple y tiene solo una variedad.
+    </div>
+    """,
+    height=600,
+)
+
 r'''
 $$\dot{x} = -(y+z)$$
 '''
